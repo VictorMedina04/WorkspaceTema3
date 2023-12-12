16f25142a81ad0399f7b72d2b2947ed15a3255ac
Victor <medina.levic24@triana.salesianos.edu>
2023-12-12 13:32:41 +0100

    Repaso

---
+++ EjemploUD3_VictorMedina/src/repaso/Principal.java
@@ -0,0 +1,10 @@
+package repaso;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Producto p = new Producto("Lechuga", 1.5, 2, true);
+
+	}
+}
---
+++ EjemploUD3_VictorMedina/src/repaso/Producto.java
@@ -0,0 +1,50 @@
+package repaso;
+
+public class Producto {
+	
+	private String nombre;
+	private double precioFabrica;
+	private int seccion;
+	private boolean enVenta;
+	
+	public Producto(String nombre, double precioFabrica, int seccion, boolean enVenta) {
+		this.nombre = nombre;
+		this.precioFabrica = precioFabrica;
+		this.seccion = seccion;
+		this.enVenta = enVenta;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecioFabrica() {
+		return precioFabrica;
+	}
+
+	public void setPrecioFabrica(double precioFabrica) {
+		this.precioFabrica = precioFabrica;
+	}
+
+	public int getSeccion() {
+		return seccion;
+	}
+
+	public void setSeccion(int seccion) {
+		this.seccion = seccion;
+	}
+
+	public boolean isEnVenta() {
+		return enVenta;
+	}
+
+	public void setEnVenta(boolean enVenta) {
+		this.enVenta = enVenta;
+	}
+	
+	
+}
