3a224a25b1c8a3be2e9c5464aff87d8c14590bd6
Victor <medina.levic24@triana.salesianos.edu>
2023-12-21 13:42:59 +0100

    examen

--- ExamenUD3_TipoB/src/ejercicio/Carrera.java
+++ ExamenUD3_TipoB/src/ejercicio/Carrera.java
@@ -18,12 +18,49 @@ public class Carrera {
 
 	}
 
-	public double calcularTiempoSegundos(double tiempoCorredor) {
+	public Corredor buscarDorsal(int dorsal) {
+		int i = 0;
+		boolean encontrado = false;
+
+		while (i < numeroCorredores && !encontrado) {
+			if (corredores[i].getDorsal() == dorsal) {
+				encontrado = true;
+			} else {
+				i++;
+			}
+		}
+		if (encontrado) {
+			return corredores[i];
+		} else {
+			return null;
+		}
+	}
+
+	public double calcularTiempoSegundos(int dorsal, double factorConversion) {
 		double tiempoCorredorSegundos;
-		int sesenta = 60;
-		tiempoCorredorSegundos = tiempoCorredor * sesenta;
+		Corredor corredor = buscarDorsal(dorsal);
+
+		tiempoCorredorSegundos = corredor.getTiempoCorredor() * factorConversion;
 
 		return tiempoCorredorSegundos;
+	}
+
+	public void mostrarArray(Corredor[] lista) {
+		for (int i = 0; i < lista.length; i++) {
+			if (lista[i] != null) {
+				System.out.println(lista[i]);
+			}
+		}
+	}
+
+	public Corredor[] buscarVeteranos() {
+		Corredor[] listaVeteranos = new Corredor[numeroCorredores];
+		for (int i = 0; i < numeroCorredores; i++) {
+			if (corredores[i].getCategoria() == 3) {
+				listaVeteranos[i] = corredores[i];
+			}
+		}
+		return listaVeteranos;
 	}
 
 }
--- ExamenUD3_TipoB/src/ejercicio/Principal.java
+++ ExamenUD3_TipoB/src/ejercicio/Principal.java
@@ -6,19 +6,19 @@ public class Principal {
 	public static void main(String[] args) {
 
 		String apellido, nombre;
-		int dorsal = 0, categoria, opcion, tam, numCorredores;
-		double tiempoCorredor, kilometros;
+		int dorsal = 0, categoria, opcion, tam, numeroCorredores = 0;
+		double tiempoCorredor, kilometros, factorConversion;
 
 		Corredor corredor;
 		Corredor[] corredores;
 
 		System.out.println("Cuántos corredores tiene la carrera");
-		numCorredores = Leer.datoInt();
-		corredores = new Corredor[numCorredores];
+		tam = Leer.datoInt();
+		corredores = new Corredor[tam];
 		System.out.println("Cuantos kilometros tiene la carrera");
 		kilometros = Leer.datoDouble();
 
-		Carrera carrera = new Carrera(corredores, numCorredores, kilometros);
+		Carrera carrera = new Carrera(corredores, numeroCorredores, kilometros);
 
 		do {
 			System.out.println("""
@@ -35,9 +35,8 @@ public class Principal {
 				System.out.println("Introduzca la categoria del corredor");
 				System.out.println(" 1-Juvenil 2-Senior 3-Veterano");
 				categoria = Leer.datoInt();
-
-				while (categoria == 1 || categoria == 2 || categoria == 3) {
-					System.out.println("Introduzca la categoria del corredor");
+				while (categoria != 1 && categoria != 2 && categoria != 3) {
+					System.out.println("Introduzca la categoria correcta");
 					System.out.println(" 1-Juvenil 2-Senior 3-Veterano");
 					categoria = Leer.datoInt();
 				}
@@ -46,14 +45,16 @@ public class Principal {
 				corredor = new Corredor(nombre, apellido, dorsal, categoria, tiempoCorredor);
 				carrera.agregarCorredor(corredor);
 				dorsal++;
-
-				for (int i = 0; i < corredores.length; i++) {
-					System.out.println(corredores[i]);
-				}
-
+				carrera.mostrarArray(corredores);
 				break;
 			case 2:
+				System.out.println("¿Cuánto es el factor de conversión?");
+				factorConversion = Leer.datoDouble();
+				System.out.println("¿Cuál es el dorsal del corredor?");
+				dorsal = Leer.datoInt();
 
+				System.out.printf("El tiempo en segundos del corredor es: %.2f segundos \n",
+						carrera.calcularTiempoSegundos(dorsal, factorConversion));
 				break;
 			}
 		} while (opcion != 0);
